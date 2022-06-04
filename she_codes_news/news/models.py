87cdc05b23d4f46a0e34350bbc6b7dfc963f7ba5
djjkc <102584654+djjkc@users.noreply.github.com>
2022-06-04 12:35:20 +1000

    Saturday class work

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,8 +1,14 @@
+from django.contrib.auth import get_user_model
+from email.mime import image
 from django.db import models
 
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
     pub_date = models.DateTimeField()
     content = models.TextField()
+    image = models.CharField(max_length=200, default= "https://pixabay.com/images/id-1489825/")
